24172a96181c402dd00e21ff4378d2fbac73e4e6
Fredrik Wollsén <fredrik@neam.se>
2014-11-18 14:43:29 +0200

    Media folder sync works regardless of whether source path was specified with a trailing slash or not

--- shell-scripts/fetch-user-generated-data.sh
+++ shell-scripts/fetch-user-generated-data.sh
@@ -86,9 +86,9 @@ if [ ! -d $dna_path/db/migration-base/$DATA/media/ ] || [ "$FORCE" ]; then
 
         export USER_GENERATED_MEDIA_S3_BUCKET=$USER_GENERATED_DATA_S3_BUCKET
         export USER_GENERATED_MEDIA_FOLDERPATH=`cat $dna_path/db/migration-base/$DATA/media.folderpath`
-        export USER_GENERATED_MEDIA_S3_URL=$USER_GENERATED_MEDIA_S3_BUCKET/$USER_GENERATED_MEDIA_FOLDERPATH
+        export USER_GENERATED_MEDIA_S3_URL=$USER_GENERATED_MEDIA_S3_BUCKET/${USER_GENERATED_MEDIA_FOLDERPATH%/}/
         mkdir $dna_path/db/migration-base/$DATA/media/ || true
-        s3cmd -v --config=/tmp/.user-generated-data.s3cfg --recursive sync "$USER_GENERATED_MEDIA_S3_URL" $dna_path/db/migration-base/$DATA/
+        s3cmd -v --config=/tmp/.user-generated-data.s3cfg --recursive sync "$USER_GENERATED_MEDIA_S3_URL" $dna_path/db/migration-base/$DATA/media/
 
         echo "User media downloaded from $USER_GENERATED_DATA_S3_URL to $dna_path/db/migration-base/$DATA/media/"
 
