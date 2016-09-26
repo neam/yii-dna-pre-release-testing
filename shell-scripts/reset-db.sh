6be0cb49230ae07f102ec992077fcb4fef7155b4
Fredrik Wollsén <fredrik@neam.se>
2016-09-26 18:35:32 +0200

    Various improvements to reset-db script, including removing unused connectionId param and preventing mysql from hanging during import

--- shell-scripts/reset-db.sh
+++ shell-scripts/reset-db.sh
@@ -12,15 +12,9 @@
 
 script_path=`dirname $0`
 
-if [ "$connectionID" == "" ]; then
-
-    echo "The environment variable connectionID needs to be set"
-    exit 1
-
-fi
-
 # fail on any error
 set -o errexit
+set -o pipefail
 
 export LOG=/tmp/reset-db.sh.log
 
@@ -64,25 +58,32 @@ if [ "$DATA" != "clean-db" ]; then
 fi
 
 echo "* Dropping all tables and views" | tee -a $LOG
-cat shell-scripts/drop-all-tables-and-views.sql | mysql -A --host=$DATABASE_HOST --port=$DATABASE_PORT --user=$DATABASE_USER --password=$DATABASE_PASSWORD $DATABASE_NAME >> $LOG
+cat shell-scripts/drop-all-tables-and-views.sql | mysql --no-auto-rehash --host=$DATABASE_HOST --port=$DATABASE_PORT --user=$DATABASE_USER --password=$DATABASE_PASSWORD $DATABASE_NAME >> $LOG
 
 echo "* Setting schema character set and collation defaults" | tee -a $LOG
 if [ -f $dna_path/db/migration-base/$DATA/alter-schema-defaults.sql ]; then
-    mysql -A --host=$DATABASE_HOST --port=$DATABASE_PORT --user=$DATABASE_USER --password=$DATABASE_PASSWORD < $dna_path/db/migration-base/$DATA/alter-schema-defaults.sql
+    mysql --no-auto-rehash --host=$DATABASE_HOST --port=$DATABASE_PORT --user=$DATABASE_USER --password=$DATABASE_PASSWORD < $dna_path/db/migration-base/$DATA/alter-schema-defaults.sql
 else
-    echo "ALTER SCHEMA $DATABASE_NAME DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_bin;" | mysql -A --host=$DATABASE_HOST --port=$DATABASE_PORT --user=$DATABASE_USER --password=$DATABASE_PASSWORD
+    echo "ALTER SCHEMA $DATABASE_NAME DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_bin;" | mysql --no-auto-rehash --host=$DATABASE_HOST --port=$DATABASE_PORT --user=$DATABASE_USER --password=$DATABASE_PASSWORD
 fi
 
-echo "* Removing DEFINER metadata from schema dump"
-sed -i -e 's/\/\*!50013 DEFINER=`[^`]*`@`[^`]*` SQL SECURITY DEFINER \*\///' $dna_path/db/migration-base/$DATA/schema.sql
+echo "* Copying schema dump to a temporary location before modifying it for successful import"
+cp $dna_path/db/migration-base/$DATA/schema.sql /tmp/$DATA.schema.sql
+
+echo "* Removing DEFINER metadata from schema dump since it prevents import of schema with other users than the one used for export"
+sed -i -e 's/\/\*!50013 DEFINER=`[^`]*`@`[^`]*` SQL SECURITY DEFINER \*\///' /tmp/$DATA.schema.sql
+
+echo "* Appending 'quit' to schema dump to prevent mysql from hanging during import"
+echo "quit" >> /tmp/$DATA.schema.sql
+echo "" >> /tmp/$DATA.schema.sql
 
 if [ "$DATA" != "clean-db" ]; then
 
     echo "* Loading the user-generated data associated with this commit" | tee -a $LOG
 
     # load mysql dumps
-    mysql -A --host=$DATABASE_HOST --port=$DATABASE_PORT --user=$DATABASE_USER --password=$DATABASE_PASSWORD $DATABASE_NAME < $dna_path/db/migration-base/$DATA/schema.sql
-    mysql -A --host=$DATABASE_HOST --port=$DATABASE_PORT --user=$DATABASE_USER --password=$DATABASE_PASSWORD $DATABASE_NAME < $dna_path/db/migration-base/$DATA/data.sql
+    mysql --no-auto-rehash --host=$DATABASE_HOST --port=$DATABASE_PORT --user=$DATABASE_USER --password=$DATABASE_PASSWORD $DATABASE_NAME < /tmp/$DATA.schema.sql
+    mysql --no-auto-rehash --host=$DATABASE_HOST --port=$DATABASE_PORT --user=$DATABASE_USER --password=$DATABASE_PASSWORD $DATABASE_NAME < $dna_path/db/migration-base/$DATA/data.sql
 
     # load user generated files (not sending --force-s3-sync to script since it is already performed above)
     shell-scripts/reset-user-generated-files.sh >> $LOG
@@ -94,13 +95,13 @@ if [ "$DATA" == "clean-db" ]; then
     echo "* Loading the clean-db data associated with this commit" | tee -a $LOG
 
     # load mysql dumps
-    mysql -A --host=$DATABASE_HOST --port=$DATABASE_PORT --user=$DATABASE_USER --password=$DATABASE_PASSWORD $DATABASE_NAME < $dna_path/db/migration-base/clean-db/schema.sql
-    mysql -A --host=$DATABASE_HOST --port=$DATABASE_PORT --user=$DATABASE_USER --password=$DATABASE_PASSWORD $DATABASE_NAME < $dna_path/db/migration-base/clean-db/data.sql
+    mysql --no-auto-rehash --host=$DATABASE_HOST --port=$DATABASE_PORT --user=$DATABASE_USER --password=$DATABASE_PASSWORD $DATABASE_NAME < /tmp/$DATA.schema.sql
+    mysql --no-auto-rehash --host=$DATABASE_HOST --port=$DATABASE_PORT --user=$DATABASE_USER --password=$DATABASE_PASSWORD $DATABASE_NAME < $dna_path/db/migration-base/clean-db/data.sql
 
 fi
 
 echo "* Running legacy Yii migrations" | tee -a $LOG
-console/yii-dna-pre-release-testing-console migrate --connectionID=$connectionID --interactive=0 >> $LOG
+console/yii-dna-pre-release-testing-console migrate --interactive=0 >> $LOG
 
 if [ -f "$dna_path/../bin/migrate-propel.sh" ]; then
 echo "* Running Propel migrations" | tee -a $LOG
@@ -108,13 +109,13 @@ $dna_path/../bin/migrate-propel.sh
 fi
 
 echo "* Loading fixtures" | tee -a $LOG
-console/yii-dna-pre-release-testing-console fixture --connectionID=$connectionID load >> $LOG
+console/yii-dna-pre-release-testing-console fixture load >> $LOG
 
 echo "* Generating database views" | tee -a $LOG
-console/yii-dna-pre-release-testing-console databaseviewgenerator --connectionID=$connectionID postResetDb >> $LOG
+console/yii-dna-pre-release-testing-console databaseviewgenerator postResetDb >> $LOG
 
 echo "* Generating database routines" | tee -a $LOG
-console/yii-dna-pre-release-testing-console databaseroutinegenerator --connectionID=$connectionID postResetDb >> $LOG
+console/yii-dna-pre-release-testing-console databaseroutinegenerator postResetDb >> $LOG
 
 if [ "$connectionID" != "dbTest" ]; then
 
